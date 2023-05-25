de7111573370a7a2a0b3004f9941619f3178a1f8
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-05-25 16:38:06 -0400

    refactored

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,26 +12,42 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const txtEditorDB = await openDB('jate', 1);
-  const transVar = txtEditorDB.transaction('jate', 'readwrite');
-  const storeVar = transVar.objectStore('jate');
-  const request = storeVar.put({ id: 1, value: content });
+  console.log('Put to the Database');
 
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  const request = store.put({id: 1,value: content});
   const result = await request;
-  console.log('Data saved to database', result.value);
+  console.log('Saved to Database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const txtEditorDB = await openDB('jate', 'readonly');
-  const storeVar = trasnVar.objectStore('jate');
-  const request = storeVar.get(1);
+  console.log('Get to the Database')
+
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.get(1);
+
+  // // Get confirmation of the request.
   const result = await request;
-  result
-    ? console.log('Data retrieved from the database', result.value)
-    : console.log('Data not found in the database');
+  console.log('result.value', result);
+  return result?.value;
 };
 
 initdb();
